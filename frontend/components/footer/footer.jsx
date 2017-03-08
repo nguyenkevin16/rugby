f950e75f27dae3a03ab57b8a6a17856655f48ac0
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-03-07 21:34:22 -0800

    Add basic google maps api for footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 
-const Footer = () => (
-  <div className='footer'>
-    <h1>2017 by UC Santa Cruz Men's Rugby.</h1>
-  </div>
-);
+class Footer extends React.Component {
+
+  componentDidMount() {
+    const mapDOMnode = document.getElementById('maps');
+    console.log(mapDOMnode);
+
+    const rugby = { lat: 36.991358, lng: -122.052097 };
+
+    const mapOptions = {
+      center: rugby,
+      zoom: 15,
+      mapTypeId: 'satellite'
+    };
+
+    const map = new google.maps.Map(mapDOMnode, mapOptions);
+
+    const marker = new google.maps.Marker({
+      position: rugby,
+      map: map
+    });
+  }
+
+  render() {
+    return (
+      <div className='footer'>
+        <div className='footer-maps'>
+          <div id='maps'>
+            Google Maps
+          </div>
+
+          <ul className='footer-address'>
+            <li> University of California, Santa Cruz </li>
+            <li> 1156 High Street </li>
+            <li> Santa Cruz, CA 95060 </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Footer;
