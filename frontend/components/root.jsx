c2cc16c8334e07003af29f81ccb1e37116eae64f
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-03-07 19:47:45 -0800

    Add basic splash image

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,11 +3,14 @@ import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import App from './app.jsx';
+import Splash from './splash/splash.jsx';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router history={hashHistory}>
-      <Route path='/' component={App}></Route>
+      <Route path='/' component={App}>
+        <IndexRoute component={Splash}/>
+      </Route>
     </Router>
   </Provider>
 );
---
+++ frontend/components/splash/splash.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Splash = () => (
+  <div className='splash'>
+    <img src='https://res.cloudinary.com/nguyenkevin16/image/upload/v1488944669/slugby_ralhup.jpg'
+      alt='Drone Overhead of the UCSC Rugby Pitch and Monterey Bay'/>
+  </div>
+);
+
+export default Splash;
