b5eafe4b07b5ff6d864d7911070bd2d28c1d0320
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-03-07 20:41:09 -0800

    Implement google fonts and basic footer/news section

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import NavBar from './navbar/navbar.jsx';
+import Footer from './footer/footer.jsx';
 
 const App = ({ children }) => (
   <div>
     <NavBar/>
     { children }
+    <Footer/>
   </div>
 );
 
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,6 +4,10 @@ const Splash = () => (
   <div className='splash'>
     <img src='https://res.cloudinary.com/nguyenkevin16/image/upload/v1488944669/slugby_ralhup.jpg'
       alt='Drone Overhead of the UCSC Rugby Pitch and Monterey Bay'/>
+
+    <div className='splash-news'>
+      News
+    </div>
   </div>
 );
 
