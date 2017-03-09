193d366c5154c124b92121b6eef9b620570368fa
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-03-08 20:02:38 -0800

    Add basic twitter feed for main updates

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 
-const Splash = () => (
-  <div className='splash'>
-    <img src='https://res.cloudinary.com/nguyenkevin16/image/upload/v1488944669/slugby_ralhup.jpg'
-      alt='Drone Overhead of the UCSC Rugby Pitch and Monterey Bay'/>
+class Splash extends React.Component {
+  render() {
+    return (
+      <div className='splash'>
+        <img src='https://res.cloudinary.com/nguyenkevin16/image/upload/v1488944669/slugby_ralhup.jpg'
+          alt='Drone Overhead of the UCSC Rugby Pitch and Monterey Bay'/>
 
-    <div className='splash-news'>
-      News
-    </div>
-  </div>
-);
+        <div className='splash-news'>
+          <a className="twitter-timeline"
+            data-width="600"
+            data-height="650"
+            data-theme="light"
+            data-link-color="#FAB81E"
+            href="https://twitter.com/MensSlugRugby">Tweets by MensSlugRugby
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Splash;
